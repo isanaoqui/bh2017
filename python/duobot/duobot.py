058a21e97c8509a3d0afe7b43afbecec5cdd3d62
chinitanao <isanaoqui@gmail.com>
2017-11-10 16:36:03 -0500

    Mixed them together

--- python/duobot/duobot.py
+++ python/duobot/duobot.py
@@ -1,25 +1,15 @@
 import battlecode
 import time
 import random
+
 import movement
+from pickup import *
+from throw import *
+
 #Start a game
 game = battlecode.Game('testplayer')
 
 start = time.clock()
-
-#define helper functions here
-def nearest_glass_state(state, entity):
-    nearest_statue = None
-    nearest_dist = 10000
-    for other_entity in state.get_entities(entity_type=battlecode.Entity.STATUE):
-        if(entity == other_entity):
-            continue
-        dist = entity.location.adjacent_distance_to(other_entity.location)
-        if(dist< nearest_dist):
-            dist = nearest_dist
-            nearest_statue = other_entity
-
-    return nearest_statue
 
 self_turn = 0
 increase_explorer = False
@@ -30,6 +20,8 @@ for state in game.turns():
     self_turn += 1
     if self_turn % 20 == 0: #increase number of explorers
         increase_explorer = True
+
+    enemies = list(state.get_entities(team=state.other_team))
 
     for entity in state.get_entities(team=state.my_team): 
         # This line gets all the bots on your team
@@ -43,31 +35,12 @@ for state in game.turns():
         if entity.id in explorer_id_list:
             movement.expansion(state,entity)
             continue
-            
-        '''
-
         
-        # code if this entity is not an explorer
-        my_location = entity.location
-        near_entites = entity.entities_within_euclidean_distance(1.9)
-        near_entites = list(filter(lambda x: x.can_be_picked, near_entites))
+        if entity.is_thrower: 
+            prep_stance(entity, 'attack', state)
+            stance(entity, 'attack', state, enemies)
 
-        for pickup_entity in near_entites:
-            assert entity.location.is_adjacent(pickup_entity.location)
-            if entity.can_pickup(pickup_entity):
-                entity.queue_pickup(pickup_entity)
-
-        statue = nearest_glass_state(state, entity)
-        if(statue != None):
-            direction = entity.location.direction_to(statue.location)
-            if entity.can_throw(direction):
-                entity.queue_throw(direction)
-
-        for direction in battlecode.Direction.directions():
-            if entity.can_move(direction):
-                assert entity.id not in explorer_id_list
-                entity.queue_move(direction)
-        '''
+            movement.away_from_glass(entity, state)
 
 
 end = time.clock()
--- python/duobot/movement.py
+++ python/duobot/movement.py
@@ -45,7 +45,30 @@ def get_unclaimed_sector_list(state):
 				sector_list.append(sector)
 	return sector_list
 
+def away_from_glass(unit, state):
+    """
+    This function instructs unit to move away from closest statue.
 
+    Returns: 
+        1 if moved away
+        -1 if moved "randomly"
+        0 if didn't move
+    """
+    statues = list(state.get_entities(entity_type='statue',team=state.my_team))
+    statues = sorted(statues, key=lambda x: unit.location.distance_to(x.location))
+
+    for statue in statues: 
+        direction = statue.location.direction_to(unit.location)
+        if unit.can_move(direction):
+            unit.queue_move(direction)
+            return 1
+
+    for direction in battlecode.Direction.directions():
+        if unit.can_move(direction):
+            unit.queue_move(direction)
+            return -1
+            
+    return 0
 
 
 
